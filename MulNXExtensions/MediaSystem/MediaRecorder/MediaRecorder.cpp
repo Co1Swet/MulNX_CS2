2194f9d4a7bce49c6a9da71016b4fccb267fc592
Co1Swet <Co1Swet@outlook.com>
2026-06-04 14:49:41 +0800

    稍微整理代码

--- MulNXExtensions/MediaSystem/MediaRecorder/MediaRecorder.cpp
+++ MulNXExtensions/MediaSystem/MediaRecorder/MediaRecorder.cpp
@@ -65,10 +65,7 @@ bool MediaRecorder::StartRecording(const std::string& filename, int w, int h) {
 
         // 写文件头（即使只有视频流）
         this->ofctx.writeHeader();
-        try {
-            this->ISys().LogWarning(std::string("输出文件头已写入，流数量=") + std::to_string(this->ofctx.streamsCount()));
-        }
-        catch (...) {}
+        this->ISys().LogWarning(std::format("输出文件头已写入，流数量={}", this->ofctx.streamsCount()));
 
         this->runFlag1 = true;
         this->ISys().LogSucc("已开始录制: " + filename);
@@ -92,16 +89,10 @@ static int64_t TimestampInMicroseconds(const av::Timestamp &ts) {
     return ts.isValid() ? ts.timestamp({1, 1000000}) : std::numeric_limits<int64_t>::max();
 }
 
-static bool PacketEarlier(const av::Packet &left, const av::Packet &right) {
-    auto lpts = TimestampInMicroseconds(left.pts());
-    auto rpts = TimestampInMicroseconds(right.pts());
-    return lpts < rpts;
-}
-
 void MediaRecorder::Encode() {
     std::vector<av::Packet> packets;
 
-    // ---------- 视频编码 ----------
+    // 视频编码
     while (auto opFrame = this->pVideoCapturer->TryPop()) {
         this->pVEncodeHelper->CheckRescaler(
             this->pVideoCapturer->stagingWidth, this->pVideoCapturer->stagingHeight,
@@ -112,7 +103,7 @@ void MediaRecorder::Encode() {
         }
     }
 
-    // ---------- 音频编码 ----------
+    // 音频编码
     while (auto opAudio = this->pAudioCapturer->TryPop()) {
         if (opAudio->samplesCount() > 0) {
             if (auto pkt = this->pAEncodeHelper->Encode(std::move(*opAudio))) {
--- MulNXExtensions/MediaSystem/MediaRecorder/MediaRecorder.hpp
+++ MulNXExtensions/MediaSystem/MediaRecorder/MediaRecorder.hpp
@@ -9,6 +9,7 @@ class MediaRecorder final :public MediaModuleBase {
     class VideoCapturer* pVideoCapturer = nullptr;
     class AudioCapturer* pAudioCapturer = nullptr;
 
+    std::filesystem::path dirVedios;
     // 音视频输出
     av::FormatContext   ofctx;
     AVRational timeBase = { 1, 60 };  // 60 fps
@@ -17,11 +18,8 @@ class MediaRecorder final :public MediaModuleBase {
     bool StartRecording(const std::string& filename, int w, int h);
     bool StopRecording();
 
-    std::filesystem::path dirVedios;
-
     void Encode();
     void ProcessMsg(MulNX::Message& msg);
-
     void Main();
 public:
     bool Init()override;
