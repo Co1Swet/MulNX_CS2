bb2cbe5ce4120724314e55e773d9a5fd75218cba
Co1Swet <Co1Swet@outlook.com>
2026-07-29 11:18:26 +0800

    再清理一些史山

--- source/MulNXExtensions/CS2/Feature/Visual/ESPBox/ESPBox.cpp
+++ source/MulNXExtensions/CS2/Feature/Visual/ESPBox/ESPBox.cpp
@@ -5,29 +5,43 @@
 void ESPBox::Draw() {
     if (!this->enable.load(std::memory_order_acquire))return;
 
-    std::shared_lock lock(this->CS2->smutex);
-    for (int i = 1; i <= 10; ++i) {
-        if (!this->CS2->GetPlayerMsg(i).Alive)continue;
-        const DirectX::XMFLOAT3 EyePos3D = this->CS2->GetPlayerMsg(i).EyePosition;
-        const DirectX::XMFLOAT3 OriginPos3D = this->CS2->GetPlayerMsg(i).Position;
+    for (int i = 0;i < 32;++i) {
+        auto* pCtrl = this->CS2->client.GetBaseEntity(i)->As<CS2::CCSPlayerController>();
+        if (!pCtrl)continue;
+        auto hPawn = MulNX::MRead(pCtrl->m_hPlayerPawn());
+        auto pPawn = this->CS2->client.GetBaseEntityFromHandle(hPawn)->As<CS2::C_CSPlayerPawn>();
+        if (!pPawn)continue;
+        if (!MulNX::MRead(pPawn->iHealth()))continue;
 
-        DirectX::XMFLOAT2 EyePos2D{};
-        DirectX::XMFLOAT2 OriginPos2D{};
+        auto pos3D = MulNX::MRead(pPawn->vOldOrigin());
+        auto eyePos3D = pos3D + MulNX::MRead(pPawn->vecViewOffset());
 
-        MulNX::Math::WorldToScreen(EyePos3D, EyePos2D, this->CS2View->GetViewMatrix(), this->CS2View->GetWinWidth(), this->CS2View->GetWinHeight());
-        MulNX::Math::WorldToScreen(OriginPos3D, OriginPos2D, this->CS2View->GetViewMatrix(), this->CS2View->GetWinWidth(), this->CS2View->GetWinHeight());
+        DirectX::XMFLOAT2 eyePos2D{};
+        DirectX::XMFLOAT2 originPos2D{};
 
-        const float hight{ ::abs(EyePos2D.y - OriginPos2D.y) * 1.25f };
+        if (!MulNX::Math::WorldToScreen(eyePos3D, eyePos2D, this->CS2View->GetViewMatrix(), this->CS2View->GetWinWidth(), this->CS2View->GetWinHeight()))
+            continue;
+        if (!MulNX::Math::WorldToScreen(pos3D, originPos2D, this->CS2View->GetViewMatrix(), this->CS2View->GetWinWidth(), this->CS2View->GetWinHeight()))
+            continue;
+
+        const float hight{ ::abs(eyePos2D.y - originPos2D.y) * 1.25f };
         const float width{ hight / 2.f };
-        const float x = EyePos2D.x - (width / 2.f);
-        const float y = EyePos2D.y - (width / 2.5f);
+        const float x = eyePos2D.x - (width / 2.f);
+        const float y = eyePos2D.y - (width / 2.5f);
 
         ImGui::GetBackgroundDrawList()->AddRect(ImVec2(x, y), ImVec2(x + width, y + hight), ImColor(0, 255, 0, 255), 0.0f, 0, 1.5f);
     }
 }
 
 bool ESPBox::Init() {
-    this->UIRegisterBackground(this->GetName(), [this](auto&&...) {return this->Draw();});
+    this->UIRegisterBackground(this->GetName(), [this](auto&&...) {
+        try {
+            this->Draw();
+        }
+        catch (MulNX::Exception& e) {
+            this->LogError(e);
+        };
+        });
 
     this->UIRegisterCallback("UI.2DVision", [this](auto&&...) {
         MulNX::UI::Checkbox("方框ESP", this->enable);
